48b2305fa89811c143cd5c8fa9937c7e83190bcf
unknown <tsetenl2@illinois.edu>
2020-08-01 16:09:59 -0400

    Add vue and vuetify

---
+++ narrative-viz/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <Chart />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Chart from './components/Chart';
+
+export default {
+  name: 'App',
+
+  components: {
+    Chart,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
